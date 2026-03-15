f4db98032ec9a92127c1af879016016a42e4fdd2
ShriNakul <shrinakul410@gmail.com>
2026-03-15 17:49:24 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>I want to see what happens</h1>
+      <h2>when i upload to github</h2>
+    </div>
+  );
+}
